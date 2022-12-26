a921e1e47b07b53474893dc38faaf28527ff7ab4
Gaurav-021 <63582549+Gaurav-021@users.noreply.github.com>
2022-12-26 17:12:13 -0600

    Add files via upload

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,26 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import frc.robot.subsystems.DrivetrainSubsystem;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Scheduler;
-
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Joystick;
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-    private static OI oi;
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  Drivetrain m_Drivetrain = new Drivetrain();
+  Joystick driver = new Joystick(0);
 
-    private static DrivetrainSubsystem drivetrain;
 
-    public static OI getOi() {
-        return oi;
-    }
 
-    @Override
-    public void robotInit() {
-        oi = new OI();
-        drivetrain = DrivetrainSubsystem.getInstance();
-    }
+  @Override
+  public void robotInit() {}
 
-    @Override
-    public void robotPeriodic() {
-        Scheduler.getInstance().run();
-    }
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    m_Drivetrain.drive(new Translation2d(driver.getX(), -1*driver.getY()), driver.getRawAxis(2), false);
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
